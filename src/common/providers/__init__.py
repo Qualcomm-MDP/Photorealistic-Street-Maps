974cd38f5eaa6d64efa8ec34c805f45833856f9e
Ashp116 <ashp116@outlook.com>
2026-03-11 12:17:44 -0400

    ADD: Overpass provider template

--- src/common/providers/__init__.py
+++ src/common/providers/__init__.py
@@ -1,4 +1,5 @@
 from .mapillary import MapillaryClient
 from .osm import OSMClient
+from .overpass import OverpassClient
 
-__all__ = ["MapillaryClient", "OSMClient"]
+__all__ = ["MapillaryClient", "OSMClient", "OverpassClient"]
---
+++ src/common/providers/overpass.py
@@ -0,0 +1,6 @@
+
+
+
+class OverpassClient:
+    def __init__(self):
+        pass
