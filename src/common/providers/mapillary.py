c7294d51fd5d2397e294bedf76908ee4560fa69b
Ashp116 <ashp116@outlook.com>
2026-03-12 11:28:04 -0400

    UPDATE: Todos

--- src/common/providers/mapillary.py
+++ src/common/providers/mapillary.py
@@ -1,6 +1,16 @@
-
+import os
+import mapillary
 
 class MapillaryClient:
     def __init__(self):
+        # NOTE: Ensure you use `os.environ['MAPILLARY_ACCESS_TOKEN']` 
+        # when you need to send the mapillary token
         pass
     
+    def fetch(self):
+        # TODO: Create a fetch function that takes in some arguments 
+        # from the user and return the Mapillary data 
+        pass
+
+    # TODO: Add more functions that is needed to process mappilary 
+    # data. Refer to https://mapillary.github.io/mapillary-python-sdk/docs/intro
--- src/common/providers/osm.py
+++ src/common/providers/osm.py
@@ -3,4 +3,11 @@
 class OSMClient:
     def __init__(self):
         pass
+
+    def fetch(self):
+        # TODO: Create a fetch function that takes in some arguments 
+        # from the user and return the OSM data 
+        pass
+
+    # TODO: Add more functions that is needed to process OSM data 
     
