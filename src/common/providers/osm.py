2db164d489ec00fa99e2b13bf0ee52c848928907
Ashp116 <ashp116@outlook.com>
2026-03-12 21:33:39 -0400

    UPDATE: Use BoundingBox dataclass

--- src/common/providers/osm.py
+++ src/common/providers/osm.py
@@ -2,7 +2,7 @@ from enum import Enum
 from typing import Any, Dict, List, Optional, Tuple
 import requests
 
-from constants import OVERPASS_URL
+from constants import OVERPASS_URL, BoundingBox
 
 # Refer https://wiki.openstreetmap.org/wiki/Map_features
 class OSM_MAP_FEATURES(Enum):
@@ -15,24 +15,16 @@ class OSMClient:
         self.timeout = timeout
 
     def fetch(self,
-        bbox: Optional[Tuple[float, float, float, float]] = None,
-        center: Optional[Tuple[float, float]] = None,
-        radius: Optional[float] = None,
+        bbox: BoundingBox,
         features: Optional[List[OSM_MAP_FEATURES]] = None,
     ) -> Dict[str, Any]:
         
-        #TODO: Create a fetch function that takes in some arguments 
-        # from the user and return the OSM data 
         if features is None:
             features = [OSM_MAP_FEATURES.BUILDING]
-
-        if bbox is None and (center is None or radius is None):
-            raise ValueError("Provide either bbox or both center and radius.")
-
         query_parts = []
 
         for feature in features:
-            query_parts.extend(self.build_feature_query(feature, bbox=bbox, center=center, radius=radius))
+            query_parts.extend(self.build_feature_query(feature, bbox=bbox))
 
         query = f"""
         [out:json][timeout:25];
@@ -55,23 +47,15 @@ class OSMClient:
     def build_feature_query(
         self,
         feature: OSM_MAP_FEATURES,
-        bbox: Optional[Tuple[float, float, float, float]] = None,
-        center: Optional[Tuple[float, float]] = None,
-        radius: Optional[float] = None,
+        bbox: BoundingBox,
     ) -> List[str]:
         """Build Overpass query snippets for a feature."""
         tag_filters = self._get_tag_filters(feature)
         query_parts = []
         
         for tag in tag_filters:
-            if bbox is not None:
-                south, west, north, east = bbox
-                area_str = f"({south},{west},{north},{east})"
-            else:
-                if center is None or radius is None:
-                    raise ValueError("Provide bbox or both center and radius.")
-                lat, lon = center
-                area_str = f"(around:{radius},{lat},{lon})"
+            south, west, north, east = bbox.min_lat, bbox.min_lon, bbox.max_lat, bbox.max_lon
+            area_str = f"({south},{west},{north},{east})"
 
             query_parts.append(f'node["{tag}"]{area_str};')
             query_parts.append(f'way["{tag}"]{area_str};')
