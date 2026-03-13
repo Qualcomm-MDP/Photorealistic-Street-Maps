b42b7a3d8f43de6ff3567b90a751e5e193656b5d
Ashp116 <ashp116@outlook.com>
2026-03-12 20:58:13 -0400

    FIX: resolve import errors

--- src/mesh_builder/extrude.py
+++ src/mesh_builder/extrude.py
@@ -1,97 +1,42 @@
-import shapely
-from shapely.geometry import Polygon
-from pyproj import Transformer
 import trimesh
-import numpy as np
-import json
-import PIL
-import matplotlib.pyplot as plt
+from common.MeshUtils import initialize_plane, get_corners, get_lines, get_height
+from common.constants import BoundingBox
 
-from common.MeshUtils import generate_plane, initialize_plane, get_corners, get_lines, get_height
 
-# Global, path to the outputted JSON file
-INPUT_JSON = "osm_data_buildings.json" 
-STREET_JSON = "osm_data_roads.json"
-SCALE = 5 # What level of precision we want
-
-WRAP_IMG = "wraps/birdseye.png"
-
-def main():
+def extrude_buildings(input_json):
     # Just helps for debugging
     # trimesh.util.attach_to_log()
-    data_buildings = None
+    scale = 5
+
+    area_bbox = BoundingBox.from_json(data=input_json)
     
-    # Read in the json data
-    with open(INPUT_JSON, "r") as f:
-        data_buildings = json.load(f)
-
-    # Declare the bounds as globals so that everyone can use them
-    global MAX_LAT
-    global MIN_LAT
-    global MAX_LON
-    global MIN_LON
-
-    # Set the global bounds
-    MAX_LAT = float(data_buildings["max_lat"])
-    MIN_LAT = float(data_buildings["min_lat"])
-    MAX_LON = float(data_buildings["max_lon"]) 
-    MIN_LON = float(data_buildings["min_lon"])
-    
-    # Initialize that initial plane, great name I know
-    hisodflkjas, plane_vertices, plane_faces = initialize_plane(data_buildings)
-    st_img = PIL.Image.open(WRAP_IMG)
-
-    # Add the satellite image of the plane in order to have some sort of a street view
-    uv = np.array([
-        [0, 0],
-        [1, 0],
-        [1, 1],
-        [0, 1]
-    ])
-
-    material = trimesh.visual.texture.SimpleMaterial(image=st_img)
-    print(material)
-
-    texture = trimesh.visual.texture.TextureVisuals(
-        uv=uv,
-        image=st_img
-    )
-
+    _, plane_vertices, plane_faces = initialize_plane(area_bbox, scale)
     mesh = trimesh.Trimesh(
         vertices=plane_vertices,
         faces=plane_faces,
-        visual=texture
     )
 
-    # buildings will be the combined mesh of everything in the scene, so the underlying plane, buildings, roads etc.
     buildings = []
-    buildings.append(mesh) # Add in the plane to begin
+    buildings.append(mesh)
 
-    # Go through the elements returned by overpass' API
-    for i, element in enumerate(data_buildings["elements"]):
+    for _, element in enumerate(input_json["elements"]):
 
         id = element["id"]
-        # Get the corners
-        corners = get_corners(element)
+        corners = get_corners(element, area_bbox, scale)
 
-        # Get the lines
         lines = get_lines(corners)
 
-        # Create the 2D Path for the mesh
         path = trimesh.path.path.Path2D(
             entities=lines,
             vertices=corners,
         )
 
-        # Get the height of the buildings
         height = get_height(element)
 
-        # Extra check to make sure that the outline of the building is a valid one
         polys = path.polygons_closed
         if not polys[0]:
-            continue # Don't try to add a mesh if it is going to be invalid
+            continue
 
-        # Get the mesh for that building
         height = -1 * height
         mesh = path.extrude(height=height)
         print(type(mesh))
@@ -123,6 +68,3 @@ def main():
     combined_mesh = trimesh.util.concatenate(buildings)
     print("Saved combined building mesh!\n")
     combined_mesh.export(f"combined.glb")
-
-if __name__ == "__main__":
-    main()
